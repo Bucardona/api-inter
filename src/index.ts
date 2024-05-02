09327a581cc1f610e01c74bfe40f112ca5b2d94f
Mona Lisa <bucardona@gmail.com>
2024-05-02 11:29:02 -0500

    añadir respuesta a rutas no definidas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { routesDms } from '@dms/routesDms'
 import { app, onStart } from '@/config/server'
+import { jsonResponseFormat } from './dms/v1/utils/jsonResponseFormat'
 
 app.use('/dms', routesDms)
+
+app.use('*', (_, res) => res.json(jsonResponseFormat(404, 'Not Found')))
 
 app.listen(Number(app.get('port')), onStart)
