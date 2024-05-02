e84787c0936127f1cf6d9e87d55c6cdf8636543c
Mona Lisa <bucardona@gmail.com>
2024-05-02 14:34:17 -0500

    actualización controlador ruta images

--- src/dms/v1/controllers/controllerDmsV1ProductsImages.ts
+++ src/dms/v1/controllers/controllerDmsV1ProductsImages.ts
@@ -3,34 +3,26 @@ import { jsonResponseFormat } from '../utils/jsonResponseFormat'
 import { execQueryDms } from '@/dms/models/dmsDatabase'
 import sharp from 'sharp'
 
-export const getDmsImagesBySku = (async (req, res) => {
-  const { codigo } = req.params // id del producto: 0 = todos, int = para un producto
+export const getDmsImageById = (async (req, res) => {
+  const { id } = req.params // id del producto: 0 = todos, int = para un producto
   try {
-    const result = await execQueryDms(`SELECT TOP 1
-    data = (
-      SELECT CAST(imagen AS varbinary(max))
-      FOR XML PATH(''), BINARY BASE64
-    )
-  FROM cot_item WHERE codigo = '${String(codigo).toUpperCase()}'`)
+    const result = await execQueryDms(`SELECT TOP 1 imagen FROM cot_item WHERE id = '${String(id).toUpperCase()}'`)
 
-    if (result && result.recordset.length > 0 && result.recordset[0].data) {
-      const { data }: { data: string } = result.recordset[0]
-      let img = Buffer.from(data, 'base64')
+    if (result && result.recordset.length > 0 && result.recordset[0].imagen) {
+      const { imagen }: { imagen: string } = result.recordset[0]
+      const { modify } = req.query as { modify?: { imgSize?: string } }
+      let img = Buffer.from(imagen)
 
       if (
-        req.query.size &&
-        Number(req.query.size) > 0 &&
-        Number(req.query.size) < 3000
+        modify?.imgSize &&
+        Number(modify.imgSize) >= 50 &&
+        Number(modify.imgSize) < 3000
       ) {
-        img = await sharp(img).resize(Number(req.query.size)).toBuffer()
+        img = await sharp(img).resize(Number(modify.imgSize)).toBuffer()
       }
-
-      // res.contentType('text/html')
-      // res.send(`<img src="data:img/png;base64,${data}">`)
       res.setHeader('Content-Length', img.length)
       res.contentType('image/jpg')
       res.send(img)
-      // res.json( jsonResponseFormat({status: 200, message: 'OK', data}) )
     } else res.json(jsonResponseFormat(400, 'Not Found'))
   } catch (error) {
     res.json(jsonResponseFormat(500, 'Error'))
@@ -66,13 +58,9 @@ export const getDmsImagesFilteringSku = (async (req, res) => {
       ) {
         img = await sharp(img).resize(Number(modify.imgSize)).toBuffer()
       }
-
-      // res.contentType('text/html')
-      // res.send(`<img src="data:img/png;base64,${data}">`)
       res.setHeader('Content-Length', img.length)
       res.contentType('image/jpg')
       res.send(img)
-      // res.json( jsonResponseFormat({status: 200, message: 'OK', data}) )
     } else res.json(jsonResponseFormat(400, 'Not Found'))
   } catch (error) {
     res.json(jsonResponseFormat(500, 'Error'))
